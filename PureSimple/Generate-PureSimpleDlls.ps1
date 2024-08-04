9224c0a272970450dfc3800f3bbe9553a482c52f
Smitis <smitis@yandex.ru>
2024-08-04 10:50:55 +0300

    Компиляция comdlg32

--- PureSimple/Generate-PureSimpleDlls.ps1
+++ PureSimple/Generate-PureSimpleDlls.ps1
@@ -12,3 +12,6 @@ $Dir64 = "$ScriptDir\bin\x64"
 
 .\Compile-ProxyDll "PureSimple" "AdvApi32" -InternalName "advapi32-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "AdvApi32-1" -C "PORTABLE_REGISTRY=1"
 .\Compile-ProxyDll "PureSimple" "AdvApi32" -InternalName "advapi32-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "AdvApi32-2" -C "PORTABLE_REGISTRY=2"
+
+.\Compile-ProxyDll "PureSimple" "ComDlg32" -InternalName "comdlg32-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "ComDlg32-1" -C "PORTABLE_REGISTRY=1"
+.\Compile-ProxyDll "PureSimple" "ComDlg32" -InternalName "comdlg32-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "ComDlg32-2" -C "PORTABLE_REGISTRY=2"
