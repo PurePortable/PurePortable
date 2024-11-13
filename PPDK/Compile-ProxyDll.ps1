5ea800eb7541ff828c5cbb02f1929a73fe39c397
Smitis <smitis@yandex.ru>
2024-11-13 16:15:10 +0300

    Доработка скриптов компиляции

--- PPDK/Compile-ProxyDll.ps1
+++ PPDK/Compile-ProxyDll.ps1
@@ -1,45 +1,45 @@
 ﻿param (
-	[Parameter(Mandatory=$true,Position=0)]
-	[Alias('S')] [string] $Src
-	,
-	[Parameter(Mandatory=$true,Position=1)]
-	[Alias('P')] [string] $Proxy
-	,
-	[Parameter(Mandatory=$false,Position=2)]
-	[Alias('R')] [string] $RC
-	,
-	[Parameter(Mandatory=$false)]
-	[Alias('O')] [string] $Out
-	,
-	[Parameter(Mandatory=$false)]
-	[Alias('C')] [string[]] $ConstList
-	,
-	[Parameter(Mandatory=$false)]
-	[Alias('IN')] [string] $InternalName
-	,
-	[Parameter(Mandatory=$false)]
-	[Alias('OF')] [string] $OriginalFilename
-	,
-	[Parameter(Mandatory=$false)]
-	[Alias('FD')] [string] $FileDescription
-	,
-	[Parameter(Mandatory=$false)]
-	[Alias('CO','Comments','Comment')] [string] $FileComment
-	,
-	[Parameter(Mandatory=$false)]
-	[switch] $x32
-	,
-	[Parameter(Mandatory=$false)]
-	[switch] $x64
-	,
-	[Parameter(Mandatory=$false)]
-	[string] $Dir32
-	,
-	[Parameter(Mandatory=$false)]
-	[string] $Dir64
-	,
-	[Parameter(Mandatory=$false)]
-	[Alias('CE')] [switch] $CorrectExport
+  [Parameter(Mandatory=$true,Position=0)]
+  [Alias('S')] [string] $Src
+  ,
+  [Parameter(Mandatory=$true,Position=1)]
+  [Alias('P')] [string] $Proxy
+  ,
+  [Parameter(Mandatory=$false,Position=2)]
+  [Alias('R')] [string] $RC
+  ,
+  [Parameter(Mandatory=$false)]
+  [Alias('O')] [string] $Out
+  ,
+  [Parameter(Mandatory=$false)]
+  [Alias('C')] [string[]] $ConstList
+  ,
+  [Parameter(Mandatory=$false)]
+  [Alias('IN')] [string] $InternalName
+  ,
+  [Parameter(Mandatory=$false)]
+  [Alias('OF')] [string] $OriginalFilename
+  ,
+  [Parameter(Mandatory=$false)]
+  [Alias('FD')] [string] $FileDescription
+  ,
+  [Parameter(Mandatory=$false)]
+  [Alias('CO','Comments','Comment')] [string] $FileComment
+  ,
+  [Parameter(Mandatory=$false)]
+  [switch] $x32
+  ,
+  [Parameter(Mandatory=$false)]
+  [switch] $x64
+  ,
+  [Parameter(Mandatory=$false)]
+  [string] $Dir32
+  ,
+  [Parameter(Mandatory=$false)]
+  [string] $Dir64
+  ,
+  [Parameter(Mandatory=$false)]
+  [Alias('CE')] [switch] $CorrectExport
 )
 #$ScriptDir = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
 
@@ -52,8 +52,8 @@ $CorrectExportC = ".\PPCorrectExportC.exe"
 . "$PSScriptRoot\Compile-ProxyDll-Settings.ps1"
 
 if ($Compiler32 -eq "" -or $Compiler64 -eq "") {
-	Write-Error "The compiler was not found"
-	exit
+  Write-Error "The compiler was not found"
+  exit
 }
 
 $SrcTmp = "~tmp.pb"
@@ -63,13 +63,13 @@ $SrcFile = "$Src.pb"
 
 # Имя результирующего файла
 if ($PSBoundParameters.ContainsKey('Out')) {
-	$OutFile = $Out
+  $OutFile = $Out
 }
 else {
-	$OutFile = $Proxy
+  $OutFile = $Proxy
 }
 if ([System.IO.Path]::GetExtension($OutFile) -eq "") {
-	$OutFile += ".dll"
+  $OutFile += ".dll"
 }
 $OutName = [System.IO.Path]::GetFileNameWithoutExtension($OutFile)
 Write-Host "Compile $OutName" -bac DarkBlue
@@ -77,112 +77,113 @@ Write-Host "Compile $OutName" -bac DarkBlue
 # Список констант, заменяемых в исходном файле
 $Constants = @{}
 if ($PSBoundParameters.ContainsKey('ConstList')) {
-	foreach ($Const in $ConstList) {
-		$c,$v = $Const -split "="
-		$Constants += @{ $c = $v }
-	}
+  foreach ($Const in $ConstList) {
+    $c,$v = $Const -split "="
+    $Constants += @{ $c = $v }
+  }
 }
 
 # Формирование временного исходного файла
 Set-Content -Lit $SrcTmp -Force -Value "" -Enc UTF8 -NoNewLine
 Get-Content -Lit $SrcFile -Enc UTF8 | foreach {
-	$s = $_
-	if ($s -match '^#PROXY_DLL\s+=\s+"[^"]+"') { # Константа PROXY_DLL на особом счету
-		$s = $s -replace '^(#PROXY_DLL\s+=\s+)"[^"]+"',"`$1`"$Proxy`""
-	}
-	elseif ($s -match '^#(?<Const>[a-zA-Z_0-9]+)(?<Space>\s*=\s*")(?<Value>[^"]+)(?<Tail>".*)') { # Остальные строковые константы
-		if ($Constants.ContainsKey($Matches.Const)) {
-			$ConstName = $Matches.Const
-			$ConstValue = $Constants[$ConstName]
-			#echo "$ConstName :: $ConstValue"
-			$s = "#$($Matches.Const)$($Matches.Space)$ConstValue$($Matches.Tail)"
-			#echo "S: $s"
-		}
-	}
-	elseif ($s -match '^#(?<Const>[a-zA-Z_0-9]+)(?<Space>\s*=\s*)(?<Value>\$?[0-9a-f]+)(?<Tail>.*)') { # Остальные числовые константы
-		if ($Constants.ContainsKey($Matches.Const)) {
-			$ConstName = $Matches.Const
-			$ConstValue = $Constants[$ConstName]
-			#echo "$ConstName :: $ConstValue"
-			$s = "#$($Matches.Const)$($Matches.Space)$ConstValue$($Matches.Tail)"
-			#echo "S: $s"
-		}
-		#echo ($Matches.Const)
-	}
-	Add-Content -Lit $SrcTmp -Value $s -Enc UTF8
+  $s = $_
+  if ($s -match '^#PROXY_DLL\s+=\s+"[^"]+"') { # Константа PROXY_DLL на особом счету
+    $s = $s -replace '^(#PROXY_DLL\s+=\s+)"[^"]+"',"`$1`"$Proxy`""
+  }
+  elseif ($s -match '^#(?<Const>[a-zA-Z_0-9]+)(?<Space>\s*=\s*")(?<Value>[^"]+)(?<Tail>".*)') { # Остальные строковые константы
+    if ($Constants.ContainsKey($Matches.Const)) {
+      $ConstName = $Matches.Const
+      $ConstValue = $Constants[$ConstName]
+      #echo "$ConstName :: $ConstValue"
+      $s = "#$($Matches.Const)$($Matches.Space)$ConstValue$($Matches.Tail)"
+      #echo "S: $s"
+    }
+  }
+  #elseif ($s -match '^#(?<Const>[a-zA-Z_0-9]+)(?<Space>\s*=\s*)(?<Value>\$?[0-9a-f]+)(?<Tail>.*)') { # Остальные числовые константы
+  elseif ($s -match '^#(?<Const>[a-zA-Z_0-9]+)(?<Space>\s*=\s*)(?<Value>\[$#]?[_0-9a-z]+)(?<Tail>.*)') { # Остальные числовые константы
+    if ($Constants.ContainsKey($Matches.Const)) {
+      $ConstName = $Matches.Const
+      $ConstValue = $Constants[$ConstName]
+      #echo "$ConstName :: $ConstValue"
+      $s = "#$($Matches.Const)$($Matches.Space)$ConstValue$($Matches.Tail)"
+      #echo "S: $s"
+    }
+    #echo ($Matches.Const)
+  }
+  Add-Content -Lit $SrcTmp -Value $s -Enc UTF8
 }
 
 # Имя файла ресурсов. По умолчанию имя исходного файла с расширением .rc
 if ($RC) {
-	$RcFile = "$RC.rc"
+  $RcFile = "$RC.rc"
 }
 else {
-	$RcFile = "$Src.rc"
+  $RcFile = "$Src.rc"
 }
 # Параметры для файла ресурсов
 if (-not $PSBoundParameters.ContainsKey('InternalName')) {
-	$InternalName = $Proxy
+  $InternalName = $Proxy
 }
 if (-not $PSBoundParameters.ContainsKey('OriginalFilename')) {
-	$OriginalFilename = $Proxy
+  $OriginalFilename = $Proxy
 }
 
 # Формирование временного файла ресурсов
 # https://learn.microsoft.com/en-us/windows/win32/menurc/stringfileinfo-block
 Set-Content -Lit $RcTmp -Force -Value "" -Enc Unicode
 Get-Content -Lit $RcFile -Enc Unicode | foreach {
-	$s = $_
-	if ($s -match '^\s*VALUE "InternalName"' -and $InternalName -ne "") {
-		$s = $s -replace ',(?<Space>\s*).*',",`"$($Matches.Space)$InternalName\0`""
-	}
-	elseif ($s -match '^\s*VALUE "OriginalFilename"' -and $OriginalFilename -ne "") {
-		$s = $s -replace ',(?<Space>\s*).*',",`"$($Matches.Space)$OriginalFilename\0`""
-	}
-	elseif ($s -match '^\s*VALUE "FileDescription"' -and $FileDescription -ne "") {
-		$s = $s -replace ',(?<Space>\s*).*',",`"$($Matches.Space)$FileDescription\0`""
-	}
-	elseif ($s -match '^\s*VALUE "Comments"' -and $FileComments -ne "") {
-		$s = $s -replace ',(?<Space>\s*).*',",`"$($Matches.Space)$FileComment\0`""
-	}
-	Add-Content -Lit $RcTmp -Value $s -Enc Unicode
+  $s = $_
+  if ($s -match '^\s*VALUE "InternalName"' -and $InternalName -ne "") {
+    $s = $s -replace ',(?<Space>\s*).*',",`"$($Matches.Space)$InternalName\0`""
+  }
+  elseif ($s -match '^\s*VALUE "OriginalFilename"' -and $OriginalFilename -ne "") {
+    $s = $s -replace ',(?<Space>\s*).*',",`"$($Matches.Space)$OriginalFilename\0`""
+  }
+  elseif ($s -match '^\s*VALUE "FileDescription"' -and $FileDescription -ne "") {
+    $s = $s -replace ',(?<Space>\s*).*',",`"$($Matches.Space)$FileDescription\0`""
+  }
+  elseif ($s -match '^\s*VALUE "Comments"' -and $FileComments -ne "") {
+    $s = $s -replace ',(?<Space>\s*).*',",`"$($Matches.Space)$FileComment\0`""
+  }
+  Add-Content -Lit $RcTmp -Value $s -Enc Unicode
 }
 
 # Компиляция x32
 if ($x32 -or ((-not $x32) -and (-not $x64))) {
-	Write-Host ""
-	if ($Dir32) {
-		$SubDir = $Dir32
-		if (-not (Test-Path $SubDir -Type Container)) {
-			New-Item $SubDir -Type Directory
-		}
-	}
-	if ((& $Compiler32 /dll /optimizer /thread /output "$SubDir\$OutFile" /resource "$RcTmp" "$SrcTmp") -ne 0) {
-		$RetCode += 1
-	}
-	if ($CorrectExport) {
-		& $CorrectExportC "$SubDir\$OutFile"
-	}
-	Remove-Item -Lit "$SubDir\$OutName.exp" -Force -ErrorAction Ignore
-	Remove-Item -Lit "$SubDir\$OutName.lib" -Force -ErrorAction Ignore
+  Write-Host ""
+  if ($Dir32) {
+    $SubDir = $Dir32
+    if (-not (Test-Path $SubDir -Type Container)) {
+      New-Item $SubDir -Type Directory | Out-Null
+    }
+  }
+  if ((& $Compiler32 /dll /optimizer /thread /output "$SubDir\$OutFile" /resource "$RcTmp" "$SrcTmp") -ne 0) {
+    $RetCode += 1
+  }
+  if ($CorrectExport) {
+    & $CorrectExportC "$SubDir\$OutFile"
+  }
+  Remove-Item -Lit "$SubDir\$OutName.exp" -Force -ErrorAction Ignore
+  Remove-Item -Lit "$SubDir\$OutName.lib" -Force -ErrorAction Ignore
 }
 
 # Компиляция x64
 if ($x64 -or ((-not $x32) -and (-not $x64))) {
-	Write-Host ""
-	if ($Dir64) {
-		$SubDir = $Dir64
-		if (-not (Test-Path $SubDir -Type Container)) {
-			New-Item $SubDir -Type Directory
-		}
-	}
-	if ((& $Compiler64 /dll /optimizer /thread /output "$SubDir\$OutFile" /resource "$RcTmp" "$SrcTmp") -ne 0) {
-		$RetCode += 2
-	}
-	if ($CorrectExport) {
-		& $CorrectExportC "$SubDir\$OutFile"
-	}
-	Remove-Item -Lit "$SubDir\$OutName.exp" -Force -ErrorAction Ignore
-	Remove-Item -Lit "$SubDir\$OutName.lib" -Force -ErrorAction Ignore
+  Write-Host ""
+  if ($Dir64) {
+    $SubDir = $Dir64
+    if (-not (Test-Path $SubDir -Type Container)) {
+      New-Item $SubDir -Type Directory | Out-Null
+    }
+  }
+  if ((& $Compiler64 /dll /optimizer /thread /output "$SubDir\$OutFile" /resource "$RcTmp" "$SrcTmp") -ne 0) {
+    $RetCode += 2
+  }
+  if ($CorrectExport) {
+    & $CorrectExportC "$SubDir\$OutFile"
+  }
+  Remove-Item -Lit "$SubDir\$OutName.exp" -Force -ErrorAction Ignore
+  Remove-Item -Lit "$SubDir\$OutName.lib" -Force -ErrorAction Ignore
 }
 
 # Завершение
---
+++ PureSimple/Generate-PureSimpleDlls-Full.ps1
@@ -0,0 +1,39 @@
+﻿$ScriptDir = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
+Set-Location -Lit $ScriptDir
+[System.IO.Directory]::SetCurrentDirectory($ScriptDir)
+
+$Dir32 = "$PSScriptRoot\bin\x32f"
+$Dir64 = "$PSScriptRoot\bin\x64f"
+
+Import-Module "..\PPDK\Compile-ProxyDll.psm1" -Force -DisableNameChecking #-ErrorAction SilentlyContinue
+Compile-ProxyDll-Start
+
+@(
+  "winmm", "version", "version+"
+  "avifil32", "dbghelp", "ddraw", "dinput", "dnsapi", "dwmapi", "glu32", "iphlpapi", "ktmw32"
+  "mpr", "msacm32", "mscms", "msi", "msvbvm60", "msvfw32", "netapi32", "opengl32", "secur32"
+  "shfolder", "urlmon", "userenv", "uxtheme", "wer", "winhttp", "wininet", "wtsapi32", "vcruntime140"
+  "comdlg32", "wintrust"
+) | foreach {
+  $name = $_
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-0" -C "PORTABLE_REGISTRY=0"
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-1" -C "PORTABLE_REGISTRY=1"
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-2" -C "PORTABLE_REGISTRY=2"
+}
+# Требующие коррекцию экспорта
+@(
+  "msimg32"
+  "shell32", "user32", "advapi32", "kernel32"
+  #"comctl32"
+) | foreach { 
+  $name = $_
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-0" -C "PORTABLE_REGISTRY=0" -CorrectExport
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-1" -C "PORTABLE_REGISTRY=1" -CorrectExport
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-2" -C "PORTABLE_REGISTRY=2" -CorrectExport
+}
+# С нестандартным расширением
+Compile-ProxyDll "PureSimple" "winspool" -InternalName "winspool-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winspool-0.drv" -C "PORTABLE_REGISTRY=0"
+Compile-ProxyDll "PureSimple" "winspool" -InternalName "winspool-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winspool-1.drv" -C "PORTABLE_REGISTRY=1"
+Compile-ProxyDll "PureSimple" "winspool" -InternalName "winspool-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winspool-2.drv" -C "PORTABLE_REGISTRY=2"
+
+Compile-ProxyDll-Result
