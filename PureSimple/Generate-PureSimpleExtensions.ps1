a6ee0c82747ccafeced2cb63478723192610927c
Smitis <smitis@yandex.ru>
2025-04-02 12:29:50 +0300

    PureSimple: Исходный код расширений перемещён в папку

--- PureSimple/Generate-PureSimpleExtensions.ps1
+++ PureSimple/Generate-PureSimpleExtensions.ps1
@@ -1,14 +1,15 @@
 ﻿$ScriptDir = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
-Set-Location -Lit $ScriptDir
-[System.IO.Directory]::SetCurrentDirectory($ScriptDir)
+Set-Location -Lit "$ScriptDir\extensions"
+[System.IO.Directory]::SetCurrentDirectory("$ScriptDir\extensions")
 
 $Dir32 = "$PSScriptRoot\bin\x32\extensions"
 $Dir64 = "$PSScriptRoot\bin\x64\extensions"
+$RcFile = "$PSScriptRoot\PureSimpleExtension.rc"
 
-Import-Module "..\PPDK\Compile-ProxyDll.psm1" -Force -DisableNameChecking #-ErrorAction SilentlyContinue
+Import-Module "$PSScriptRoot\..\PPDK\Compile-ProxyDll.psm1" -Force -DisableNameChecking #-ErrorAction SilentlyContinue
 Compile-ProxyDll-Start
 
-Compile-ProxyDll "PureExtensionIni" "PurePort" -RC "PureSimpleExtension" -IN "PurePortIni" -OF "PurePortIni" -FD "PurePortableSimpleExtension: Modify configs (ini, xml, json)" -Dir32 $Dir32 -Dir64 $Dir64 -O "PurePortIni"
-Compile-ProxyDll "PureExtensionMFO" "PurePort" -RC "PureSimpleExtension" -IN "PurePortMFO" -OF "PurePortMFO" -FD "PurePortableSimpleExtension: Monitoring File Operations" -Dir32 $Dir32 -Dir64 $Dir64 -O "PurePortMFO"
+Compile-ProxyDll "PureExtensionIni" "PurePort" -RC $RcFile -IN "PurePortIni" -OF "PurePortIni" -FD "PurePortableSimpleExtension: Modify configs (ini, xml, json)" -Dir32 $Dir32 -Dir64 $Dir64 -O "PurePortIni"
+Compile-ProxyDll "PureExtensionMFO" "PurePort" -RC $RcFile -IN "PurePortMFO" -OF "PurePortMFO" -FD "PurePortableSimpleExtension: Monitoring File Operations" -Dir32 $Dir32 -Dir64 $Dir64 -O "PurePortMFO"
 
 Compile-ProxyDll-Result
