027252bfa214d4abae373004f9a620dcf1223d81
Smitis <smitis@yandex.ru>
2025-04-04 15:07:28 +0300

    Доработка скриптов компиляции

--- PureSimple/Generate-PureSimpleLoaders.ps1
+++ PureSimple/Generate-PureSimpleLoaders.ps1
@@ -1,4 +1,8 @@
-﻿$Dir32 = "$PSScriptRoot\bin\x32\loaders"
+﻿$ScriptDir = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
+Set-Location -Lit $ScriptDir
+[System.IO.Directory]::SetCurrentDirectory($ScriptDir)
+
+$Dir32 = "$PSScriptRoot\bin\x32\loaders"
 $Dir64 = "$PSScriptRoot\bin\x64\loaders"
 
 Import-Module "..\PPDK\Compile-ProxyDll.psm1" -Force -DisableNameChecking #-ErrorAction SilentlyContinue
