478cb71d84dc9fdeb324db4cfa915d6716c010f4
Smitis <smitis@yandex.ru>
2026-08-12 12:23:59 +0300

    PureSimple: Доработки скриптов

--- PureSimple/Generate-PureSimpleDlls-Full.ps1
+++ PureSimple/Generate-PureSimpleDlls-Full.ps1
@@ -8,32 +8,43 @@ $Dir64 = "$PSScriptRoot\bin\x64f"
 Import-Module "..\PPDK\Compile-ProxyDll.psm1" -Force -DisableNameChecking #-ErrorAction SilentlyContinue
 Compile-ProxyDll-Start
 
-@(
+$L1 = @(
   "winmm", "version", "version+"
   "avifil32", "dbghelp", "ddraw", "dinput", "dnsapi", "dwmapi", "dwrite", "glu32", "iphlpapi", "ktmw32"
   "mpr", "msacm32", "mscms", "msi", "msvbvm60", "msvfw32", "netapi32", "opengl32", "propsys", "secur32"
   "shfolder", "urlmon", "userenv", "uxtheme", "wer", "winhttp", "wininet", "wtsapi32", "vcruntime140"
-  "comdlg32", "wintrust"
-) | foreach {
-  $name = $_
+  "comdlg32", "wintrust", "shell32", "advapi32"
+)
+$L2 = @( # Требующие коррекцию экспорта
+  "msimg32", "kernel32", "user32"
+)
+$L3 = @( # С нестандартным расширением
+  @{name="winspool";ext="drv"}
+)
+$N = $L1.Length + $L2.Length + $L3.Length
+$I = 0
+foreach ($name in $L1) {
+  $I++
+  Write-Host "$I / $N" -Bac DarkBlue
   Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-0" -C "PORTABLE_REGISTRY=0"
   Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-1" -C "PORTABLE_REGISTRY=1"
   Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-2" -C "PORTABLE_REGISTRY=2"
 }
-# Требующие коррекцию экспорта
-@(
-  "msimg32"
-  "shell32", "user32", "advapi32", "kernel32"
-  #"comctl32"
-) | foreach { 
-  $name = $_
+foreach ($name in $L2) { 
+  $I++
+  Write-Host "$I / $N" -Bac DarkBlue
   Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-0" -C "PORTABLE_REGISTRY=0" -CorrectExport
   Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-1" -C "PORTABLE_REGISTRY=1" -CorrectExport
   Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-2" -C "PORTABLE_REGISTRY=2" -CorrectExport
 }
-# С нестандартным расширением
-Compile-ProxyDll "PureSimple" "winspool" -InternalName "winspool-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winspool-0.drv" -C "PORTABLE_REGISTRY=0"
-Compile-ProxyDll "PureSimple" "winspool" -InternalName "winspool-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winspool-1.drv" -C "PORTABLE_REGISTRY=1"
-Compile-ProxyDll "PureSimple" "winspool" -InternalName "winspool-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winspool-2.drv" -C "PORTABLE_REGISTRY=2"
+foreach ($src in $L3) {
+  $name = $src.name
+  $ext = $src.ext
+  $I++
+  Write-Host "$I / $N" -Bac DarkBlue
+  Compile-ProxyDll "PureSimple" "winspool" -InternalName "$name-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-0.$ext" -C "PORTABLE_REGISTRY=0"
+  Compile-ProxyDll "PureSimple" "winspool" -InternalName "$name-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-1.$ext" -C "PORTABLE_REGISTRY=1"
+  Compile-ProxyDll "PureSimple" "winspool" -InternalName "$name-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-2.$ext" -C "PORTABLE_REGISTRY=2"
+}
 
 Compile-ProxyDll-Result
--- PureSimple/Generate-PureSimpleDlls.ps1
+++ PureSimple/Generate-PureSimpleDlls.ps1
@@ -8,26 +8,28 @@ $Dir64 = "$PSScriptRoot\bin\x64"
 Import-Module "..\PPDK\Compile-ProxyDll.psm1" -Force -DisableNameChecking #-ErrorAction SilentlyContinue
 Compile-ProxyDll-Start
 
-Compile-ProxyDll "PureSimple" "pureport" -InternalName "pureport-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "PurePort-0" -C "PORTABLE_REGISTRY=0"
-Compile-ProxyDll "PureSimple" "pureport" -InternalName "pureport-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "PurePort-1" -C "PORTABLE_REGISTRY=1"
-Compile-ProxyDll "PureSimple" "pureport" -InternalName "pureport-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "PurePort-1xp" -C "PORTABLE_REGISTRY=1","PROXY_DLL_COMPATIBILITY=5" -x32
-Compile-ProxyDll "PureSimple" "pureport" -InternalName "pureport-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "PurePort-2" -C "PORTABLE_REGISTRY=2"
-
-Compile-ProxyDll "PureSimple" "winmm" -InternalName "winmm-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winmm-0" -C "PORTABLE_REGISTRY=0"
-Compile-ProxyDll "PureSimple" "winmm" -InternalName "winmm-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winmm-1" -C "PORTABLE_REGISTRY=1"
-Compile-ProxyDll "PureSimple" "winmm" -InternalName "winmm-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "winmm-2" -C "PORTABLE_REGISTRY=2"
-
-Compile-ProxyDll "PureSimple" "version" -InternalName "version-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "version-0" -C "PORTABLE_REGISTRY=0"
-Compile-ProxyDll "PureSimple" "version" -InternalName "version-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "version-1" -C "PORTABLE_REGISTRY=1"
-Compile-ProxyDll "PureSimple" "version" -InternalName "version-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "version-2" -C "PORTABLE_REGISTRY=2"
-
-Compile-ProxyDll "PureSimple" "advapi32" -InternalName "advapi32-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "advapi32-1" -C "PORTABLE_REGISTRY=1"
-Compile-ProxyDll "PureSimple" "advapi32" -InternalName "advapi32-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "advapi32-2" -C "PORTABLE_REGISTRY=2"
-
-Compile-ProxyDll "PureSimple" "comdlg32" -InternalName "comdlg32-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "comdlg32-1" -C "PORTABLE_REGISTRY=1"
-Compile-ProxyDll "PureSimple" "comdlg32" -InternalName "comdlg32-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "comdlg32-2" -C "PORTABLE_REGISTRY=2"
-
-Compile-ProxyDll "PureSimple" "kernel32" -InternalName "kernel32-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "kernel32-1" -C "PORTABLE_REGISTRY=1" -CorrectExport
-Compile-ProxyDll "PureSimple" "kernel32" -InternalName "kernel32-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "kernel32-2" -C "PORTABLE_REGISTRY=2" -CorrectExport
+$L1 = @(
+  "pureport", "winmm", "version"
+  "advapi32", "comdlg32"
+)
+$L2 = @( # Требующие коррекцию экспорта
+  "kernel32"
+)
+$N = $L1.Length + $L2.Length
+$I = 0
+foreach ($name in $L1) {
+  $I++
+  Write-Host "$I / $N" -Bac DarkBlue
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-0" -C "PORTABLE_REGISTRY=0"
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-1" -C "PORTABLE_REGISTRY=1"
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-2" -C "PORTABLE_REGISTRY=2"
+}
+foreach ($name in $L2) { 
+  $I++
+  Write-Host "$I / $N" -Bac DarkBlue
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-0" -FileDescription "PureSimple without Registry module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-0" -C "PORTABLE_REGISTRY=0" -CorrectExport
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-1" -FileDescription "PureSimple with Registry 1 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-1" -C "PORTABLE_REGISTRY=1" -CorrectExport
+  Compile-ProxyDll "PureSimple" "$name" -InternalName "$name-2" -FileDescription "PureSimple with Registry 2 module" -Dir32 $Dir32 -Dir64 $Dir64 -O "$name-2" -C "PORTABLE_REGISTRY=2" -CorrectExport
+}
 
 Compile-ProxyDll-Result
